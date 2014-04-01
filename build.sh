a4b890484b771d392735167552c3117ed53f7af0
Chris Roemmich <chris@logicalgrape.com>
2014-04-01 16:16:05 -0500

    deploy to logicalgrape nexus

--- build.sh
+++ build.sh
@@ -9,10 +9,11 @@ fi
 version=$1
 ./gradlew clean sources javadocJarRelease && git checkout gh-pages && mvn install:install-file \
 	-Dfile=build/libs/facebook-api-android-aar.aar\
-	-DgroupId=com.facebook\
-	-DartifactId=facebook-android-sdk\
+	-DrepositoryId=nexus.logicalgrape.com\
+	-Durl=http://nexus.logicalgrape.com/content/repositories/releases\
+	-DgroupId=com.facebook.android\
+	-DartifactId=facebook\
 	-Dversion=$version\
 	-Dpackaging=aar\
 	-Dsources=build/libs/facebook-api-android-aar-sources.jar\
 	-Djavadoc=build/libs/facebook-api-android-aar-javadoc.jar\
-	-DlocalRepositoryPath=./
